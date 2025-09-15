8c2f2cc3c1e46e1713c1c59a99638b10a9b05a80
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2025-09-15 11:42:44 +0700

    Radiant: WIP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Copyright (C) 2024-2025 The Android Open Source Project
 # Copyright (C) 2024-2025 SebaUbuntu's TWRP device tree generator
@@ -6,68 +7,69 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-FDEVICE="radiant"
+# Locale & Device Configuration
+export LC_ALL="C"
+export TARGET_DEVICE_ALT="X695D,X695,Infinix-X695D,Infinix-X695"
+export FOX_TARGET_DEVICES="X695C,X695D,X695,Infinix-X695C,Infinix-X695D,Infinix-X695"
 
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
-	if [ -n "$chkdev" ]; then
-		FOX_BUILD_DEVICE="$FDEVICE"
-else
-chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
-	[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-fi
-}
+# Partitions Recovery
+export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/by-name/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/by-name/vendor"
+    
+# Device Partition Setup
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-	fox_get_target_device
-fi
+# Build Optimizations
+export FOX_DELETE_AROMAFM=1
+export FOX_REMOVE_AAPT=1
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export FOX_BUILD_TYPE="Radiant"
-	export FOX_VERSION="R11.1_1"
-	export FOX_VARIANT="XOS"
-	export OF_MAINTAINER="excaliburXD"
-	export OF_DEVICE_ALT="X695C,X695D,X695"
-	export LC_ALL="C"
-	export OF_AB_DEVICE=1
-	export OF_PATCH_AVB20=1
-	export OF_QUICK_BACKUP_LIST="/data;"
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export FOX_DELETE_AROMAFM=1
-	export OF_USE_GREEN_LED=0
-	export FOX_ENABLE_APP_MANAGER=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_ENABLE_LPTOOLS=1
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	export OF_DISABLE_FORCED_ENCRYPTION=1
-	export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
-	export OF_SCREEN_H=2460
-	export OF_STATUS_H=80
-	export OF_CLOCK_POS=1
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export OF_FLASHLIGHT_ENABLE=0
-	export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
-	export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
-	export OF_ADVANCED_SECURITY=1
-	export OF_RUN_POST_FORMAT_PROCESS=1
-	export OF_SKIP_ORANGEFOX_PROCESS=1
+# Feature Support
+export FOX_ENABLE_APP_MANAGER=1
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_XZ_UTILS=1
 
-if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-	export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-	export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-	export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-	export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-	fi
-fi
-#
+# Build Metadata
+export FOX_BUILD_TYPE="Stable"
+export FOX_VERSION="R11.1_2"
+export FOX_VARIANT="XOS"
+export OF_MAINTAINER="excaliburXD"
+
+# AVB & Treble
+export OF_PATCH_AVB20=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+
+# UI & Hardware Features
+export OF_USE_GREEN_LED=0
+export OF_FLASHLIGHT_ENABLE=0
+export OF_DISABLE_OTA_MENU=1
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+
+# Encryption & Magisk Handling
+export OF_DISABLE_FORCED_ENCRYPTION=1
+export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+
+# Advanced Functions
+export OF_ENABLE_LPTOOLS=1
+export OF_ADVANCED_SECURITY=1
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
+export OF_KEEP_DM_VERITY=1
+
+# UI Layout Settings
+export OF_SCREEN_H=2460
+export OF_STATUS_H=105
+export OF_STATUS_INDENT_LEFT=52
+export OF_STATUS_INDENT_RIGHT=52
+export OF_CLOCK_POS=1
+
+# Backup & Post-Flash Config
+export OF_QUICK_BACKUP_LIST="/boot;/data;"
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+export OF_RUN_POST_FORMAT_PROCESS=1
+export OF_SKIP_ORANGEFOX_PROCESS=1
